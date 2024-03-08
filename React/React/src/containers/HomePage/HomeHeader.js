833714985add18e2ab7038a6f229751437136ea5
Pham Thai Binh <thaibinh100402@gmail.com>
2024-03-08 12:50:58 +0700

    xong chuyen doi ngon ngu

--- React/React/src/containers/HomePage/HomeHeader.js
+++ React/React/src/containers/HomePage/HomeHeader.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils'
+import { changeLanguageApp } from '../../store/actions'
 
 class HomeHeader extends Component {
 
-    render() {
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppredux(language)
+        // alert(language)
+    }
 
+    render() {
+        let language = this.props.language
+        console.log('check language:', language);
         return (
             <React.Fragment>
                 <div className='home-header-container'>
@@ -35,8 +43,8 @@ class HomeHeader extends Component {
                         </div>
                         <div className='right-content'>
                             <div className='support'><i className="fas fa-question"></i><FormattedMessage id="homeheader.hoTro" /></div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div>
@@ -93,6 +101,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppredux: (languge) => dispatch(changeLanguageApp(languge)) 
     };
 };
 
